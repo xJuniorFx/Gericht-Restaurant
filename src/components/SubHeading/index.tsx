bf91d8f8cbe959081fe9d6a6a6df11d63d06b2f4
Junior <juniorfernandessilva1@gmail.com>
2023-07-26 21:12:51 -0300

    Production of Header and SubHeading - stylization and responsiveness

--- src/components/SubHeading/index.tsx
+++ src/components/SubHeading/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import style from './SubHeading.module.scss'
+import {images} from '../../constants/';
 
-const SubHeading = () => {
+interface props {
+    title: string
+}
+
+const SubHeading = ({title}: props) => {
     return(
-        <nav></nav>
+        <div className={style.header__subHeading}>
+            <p className={style.subHeading__title}>{title}</p>
+            <img src={images.spoon} alt='spoon' className='spoon__img'/>
+        </div>
     )
 }
 
--- src/container/Header/index.tsx
+++ src/container/Header/index.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import styles from './Header.module.scss'
+import style from './Header.module.scss'
+import SubHeading from '../../components/SubHeading';
+import {images} from '../../constants'
 
 const Header = () => (
-  <div>
-    Header
+  <div className={style.app__header} id='home'>
+    <div className={style.app__wrapper_info}>
+      <SubHeading title='Chase The New Flavor'/>
+      <h1 className={style.app__header_h1}>The Key to Fine Dining</h1>
+      <p className={style.app__header_p}>
+        Welcome to our restaurant, where culinary excellence meets unparalleled hospitality. 
+        Embark on a gastronomic journey like no other, as we present a symphony of flavors 
+        crafted with the finest ingredients and served with passion. Our talented chefs 
+        and dedicated staff are committed to delivering an unforgettable dining experience 
+        that will tantalize your taste buds and leave you craving for more.
+      </p>
+      <button type='button' className={style.custom__button}>Explore Menu</button>
+    </div> 
+    <div className={style.app__wrapper_img}>
+      <img src={images.welcome} alt='header img'/>
+    </div>
   </div>
 );
 
