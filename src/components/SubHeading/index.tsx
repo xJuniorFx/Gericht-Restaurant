4766880a0bcaa5b3254015750a36617cdf04f3f9
Junior <juniorfernandessilva1@gmail.com>
2023-08-14 19:31:15 -0300

    Finalization of the component SubHeading and its styles

--- src/components/SubHeading/index.tsx
+++ src/components/SubHeading/index.tsx
@@ -1,18 +1,18 @@
-import React from 'react';
-import style from './SubHeading.module.scss'
-import {images} from '../../constants/';
+import React from "react";
+import style from "./SubHeading.module.scss";
+import { images } from "../../constants/";
 
 interface props {
-    title: string
+  title: string;
 }
 
-const SubHeading = ({title}: props) => {
-    return(
-        <div className={style.header__subHeading}>
-            <p className={style.subHeading__title}>{title}</p>
-            <img src={images.spoon} alt='spoon' className='spoon__img'/>
-        </div>
-    )
-}
+const SubHeading = ({ title }: props) => {
+  return (
+    <div className={style.header__subHeading}>
+      <p className={style.subHeading__title}>{title}</p>
+      <img src={images.spoon} alt="spoon" className="spoon__img" />
+    </div>
+  );
+};
 
 export default SubHeading;
