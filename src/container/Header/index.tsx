26d9d3c7a3c4ff02acc0d2b670f370c2ccf33e91
Junior <juniorfernandessilva1@gmail.com>
2023-08-15 18:36:54 -0300

    Adjusting h1 height and styles semantics

--- src/container/Header/index.tsx
+++ src/container/Header/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import style from "./Header.module.scss";
+import styles from "./Header.module.scss";
 import SubHeading from "../../components/SubHeading";
 import { images } from "../../constants";
 
 const Header = () => (
-  <div className={style.app__header} id="home">
-    <div className={style.app__wrapper_info}>
+  <div className={styles.app__header} id="home">
+    <div className={styles.app__wrapper_info}>
       <SubHeading title="Chase The New Flavor" />
-      <h1 className={style.app__header_h1}>The Key to Fine Dining</h1>
-      <p className={style.app__header_p}>
+      <h1 className={styles.app__header_h1}>The Key to Fine Dining</h1>
+      <p className={styles.app__header_p}>
         Welcome to our restaurant, where culinary excellence meets unparalleled
         hospitality. Embark on a gastronomic journey like no other, as we
         present a symphony of flavors crafted with the finest ingredients and
@@ -16,11 +16,11 @@ const Header = () => (
         committed to delivering an unforgettable dining experience that will
         tantalize your taste buds and leave you craving for more.
       </p>
-      <button type="button" className={style.custom__button}>
+      <button type="button" className={styles.custom__button}>
         Explore Menu
       </button>
     </div>
-    <div className={style.app__wrapper_img}>
+    <div className={styles.app__wrapper_img}>
       <img src={images.welcome} alt="header img" />
     </div>
   </div>
