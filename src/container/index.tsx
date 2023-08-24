191e2f89eba1e1e7c4d647ea23ee58f70f476036
Junior <juniorfernandessilva1@gmail.com>
2023-08-24 00:33:26 -0300

    Changing folder name Findus => FindUs

--- src/container/index.tsx
+++ src/container/index.tsx
@@ -1,12 +1,12 @@
-import AboutUs from './AboutUs';
-import Chef from './Chef';
-import FindUs from './Findus';
-import Footer from './Footer';
-import Gallery from './Gallery';
-import Header from './Header';
-import Intro from './Intro';
-import Laurels from './Laurels';
-import SpecialMenu from './Menu';
+import AboutUs from "./AboutUs";
+import Chef from "./Chef";
+import FindUs from "./FindUs";
+import Footer from "./Footer";
+import Gallery from "./Gallery";
+import Header from "./Header";
+import Intro from "./Intro";
+import Laurels from "./Laurels";
+import SpecialMenu from "./Menu";
 
 export {
   AboutUs,
