40880d07a21138f2630e6ca6e3687a5e3ca2ddea
Junior <juniorfernandessilva1@gmail.com>
2023-08-24 00:44:15 -0300

    Making the Contact constant and its styles

--- src/container/Findus/index.tsx
+++ src/container/Findus/index.tsx
@@ -1,8 +1,32 @@
-import React from 'react';
+import React from "react";
+import styles from "./FindUs.module.scss";
+import SubHeading from "../../components/SubHeading";
+import { images } from "../../constants";
 
 const FindUs = () => (
-  <div>
-    FindUs
+  <div className={styles.app__contact} id="contact">
+    <div className={styles.app__contact_content}>
+      <SubHeading title="Contact" />
+      <h1 className={styles.app__contact_content_h1}>Find Us</h1>
+      <p className={styles.app__contact_content_p}>
+        Lane Ends Bungalow, Whatcroft Hall Lane, Rudheath, CW9 7SG
+      </p>
+      <div className={styles.app__contact_content_info}>
+        <h1 className={styles.contact_content_info_h1}>Opening Hours</h1>
+        <p className={styles.contact_content_info_p}>
+          Mon - Fri: 10:00 am - 02:00 am
+        </p>
+        <p className={styles.contact_content_info_p}>
+          Sat - Sun: 10:00 am - 03:00 am
+        </p>
+      </div>
+      <button type="button" className={styles.custom__button}>
+        Visit Us
+      </button>
+    </div>
+    <div className={styles.app__contact_img}>
+      <img src={images.findus} alt="Drink img" />
+    </div>
   </div>
 );
 
